e3ad8520989fe8a47faa8d55d9be4b4280954085
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-06-13 17:40:29 -0700

    Successfully grabs data from the api

--- server/index.js
+++ server/index.js
@@ -2,16 +2,34 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const axios = require('axios');
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../client/public')));
+
 
 const PORT = process.env.PORT || 2222;
 
 
 app.get('/api/ping', (req, res) => {
-  res.status(200).send("Success");
+  res.status(200).send({
+    success: 'true',
+  })
 })
 
+app.get('/api/posts', (req, res) => {
+  axios.get('http://hatchways.io/api/assessment/blog/posts')
+  .then(response => {
+    res.send(response.data)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+})
 
 app.listen(PORT, () => {
   console.log(`Web server running on: http://localhost:${PORT}`);
